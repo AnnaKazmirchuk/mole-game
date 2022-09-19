f4979e8a78da8ca533228f4073125209b261b34e
Anna Kazmirchuk <93005766+AnnaKazmirchuk@users.noreply.github.com>
2022-09-19 23:48:14 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,21 +1,19 @@
 const deadCounter = document.getElementById("dead");
 const lostCounter = document.getElementById("lost");
 
-function getHole(index) {
-    return document.getElementById(`hole${index}`)
-}
-
-for (let i = 1; i < 9; i++ ) {
-    elementHole = getHole(i);
+for (let i = 1; i < 10; i++ ) {
+    const elementHole = document.getElementById(`hole${i}`);
     elementHole.onclick = () => {
         if (elementHole.className.includes('hole_has-mole')) {
             deadCounter.textContent = Number(deadCounter.textContent) + 1;
-            if (Number(deadCounter.textContent) == 10)
-                alert("You win!");
-        } else { lostCounter.textContent = Number(lostCounter.textContent) + 1;
-            if ((Number(deadCounter.textContent) == 5))
-                alert("You lost!")
-
+        } else (lostCounter.textContent = Number(lostCounter.textContent) + 1)
+                
+        if (Number(deadCounter.textContent) == 10) {
+            alert("You win!");
+        } else if (Number(lostCounter.textContent) == 5) {
+            alert("You lost!");
         }
     }
-}
+}    
+
+
